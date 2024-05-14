89371fb81a6d89c53a6ac37a969f44e8d9b641fd
TonyXiao <Tony.Xiao@fisherpaykel.com>
2024-05-15 10:02:26 +1200

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='commsHandler',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['pyserial'],
     author='Tony Xiao',
